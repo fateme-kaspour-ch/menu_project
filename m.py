2856ff4f7653555b3affe667922e4fac678b63a8
Fateme Kaspour <fateme-kaspour-ch.com>
2023-04-11 16:33:38 +0430

    change class name from final to Menu

---
+++ m.py
@@ -0,0 +1,12 @@
+import os
+'''try:
+    main_file = input('Enter the name of file : ') + '.txt'
+    copy_file = input('Enter the name of the copy file : ') + '.txt'
+    with open(main_file, "r") as fr, open(copy_file, "w") as fw:
+        for line in fr:
+            fw.writelines(line)
+except FileNotFoundError:
+    print('There is no file with this name')'''
+t = os.path.
+f = open(t,'r')
+print(f.read())
